6b338c40d0843aff8a73514bc77341433175e64a
BoySandyUtomo <60342771+BoySandyUtomo@users.noreply.github.com>
2020-10-22 18:36:16 +0800

    Home 1.2

--- resources/views/Profile/visimisi.blade.php
+++ resources/views/Profile/visimisi.blade.php
@@ -1,28 +1,19 @@
-@extends('layout/userlayout')
+@extends('layouts/userlayout')
 @section('title', 'Fasilitas')
 
 @section('content')
 
-<div class="caption">
-    <img src="{{ asset('img/rog.jpg') }}" class="img-full-width" />
-    <h3>My Caption Goes Here</h3>
-</div>
-
-
-<div class="container">
-    <div class="row mt-4 mb-row">
-            <div class="col mx-4 col-overflow">
-                <h2>Visi</h2>
-                
-            </div>
-            
-
-
-             <div class="col mx-4 col-overflow">
-                <h2>Misi</h2>
-            </div>
-
+<div class="container-fluid">
+    <div class="row visi-misi">
+        <div class="col-sm-8">
+        asda
         </div>
+        <div class="col-sm-4">
+            2 of 3 (wider)
+        </div>
+    </div>
 </div>
+
+
 
 @endsection
--- resources/views/layouts/userlayout.blade.php
+++ resources/views/layouts/userlayout.blade.php
@@ -16,24 +16,29 @@
     <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
     <link href="{{asset('css/style.css')}}" rel="stylesheet">
     <link href="{{asset('css/style-baru.css')}}" rel="stylesheet">
+    <link href="{{asset('css/new_style.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script src="{{asset('js/javascript.js')}}"></script>
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg nav-quick text-dark background-sec">
+<nav class="navbar navbar-expand-lg nav-quick text-white navbar-color">
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Quick Menu
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="width: 250px;">
-        </div>
-      </li>
+    <ul class="navbar-nav quick-margin">
 
-      <li class="nav-item">
-            <a class="nav-link" href="">Official UB</span></a>
-        </li>
+    <li class="nav-item">
+      <a class="nav-link" href="">UB Official</span></a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="">Vokasi Official</span></a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="">BITS</span></a>
+    </li>
+
+
     </ul>
   </div>
 </nav>
@@ -45,16 +50,17 @@
         <img src="{{ asset('img/teknologi informasi.png') }}" class="kotak">
       </div>
   </div>
-</div>
 
 
-<section id="navbar" class="sticky-top">
-<nav class="navbar navbar-expand-lg navbar-dark text-light background">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+
+<section id="navbar"> 
+
+<nav class="navbar navbar-expand-lg navbar-dark text-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+  <div class="collapse navbar-collapse" id="navbarToggler">
   <ul class="navbar-nav mr-auto">
         <li class="nav-item">
             <a class="nav-link" href="{{ url('/') }}">Home</span></a>
@@ -114,150 +120,212 @@
         </li>
         </ul>
         <form class="form-inline my-2 my-lg-0  mr-5">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+
+
+        <div class="search">
+          <input class="search-text" type="text" name="" placeholder="Search">
+          <a href="#" class="search-btn"><i class="fas fa-search"></i></a>
+        </div>
+
+        
         </form>
   </div>
 </nav>
-</section>
 
+</section>
+</div>
 
 @yield('content')
 
 <!-- Footer -->
-<footer class="pt-4 background footer-text mt-5">
+<footer class=" container-fluid pt-4 background kotak-footer text-md-left footer-text mt-5">
 
   <!-- Footer Links -->
-  <div class="container text-center text-md-left">
+
 
     <!-- Grid row -->
     <div class="row">
 
       <!-- Grid column -->
-      <div class="col-md-4 mx-auto">
+      <div class="col-md-3 mx-auto">
 
-        <!-- Content -->
-        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Layanan Ub</h5>
-         <ul class="list-unstyled">
+        <!-- Links -->
+        <div class="mx-3">
+        <img src="{{ asset('img/BKSI.png') }}" width=" 100%" height="100%">
+
+        <ul class="list-unstyled mt-3">
           <li>
-            <p>Link 1</p>
-          </li>
-          <li>
-            <p>Link 1</p>
-          </li>
-          <li>
-            <p>Link 1</p>
-          </li>
-          <li>
-           <p>Link 1</p>
+            <h5>TEKNOLOGI INFORMASI</h5>
+            <h3>PENDIDIKAN VOKASI</h3>
+            <h5>UNIVERSITAS BRAWIJAYA</h5>
+            <p>Jl. Veteran No.12-13 Malang Jawa Timur</p>
+            <p>ti.vokasi@ub.ac.id</p>
           </li>
         </ul>
-
+        </div>
       </div>
       <!-- Grid column -->
 
-      <hr class="clearfix w-100 d-md-none">
-
-      <!-- Grid column -->
-
-      <hr class="clearfix w-100 d-md-none">
-
-      <!-- Grid column -->
-      <div class="col-md-2 mx-auto">
-
+      <div class="col-md-3 mx-auto">
+      <div class="mx-3">
         <!-- Links -->
-        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Blog</h5>
+        <h5 class="font-weight-bold text-uppercase mt-3">Fasilitas Kampus</h5>
+        <hr >
 
         <ul class="list-unstyled">
           <li>
-            <p>Link 1</p>
+            <p>BITS UB</p>
           </li>
           <li>
-            <p>Link 1</p>
+            <p>Digital Library</p>
           </li>
           <li>
-            <p>Link 1</p>
+            <p>Ebook Store</p>
           </li>
           <li>
-           <p>Link 1</p>
+            <p>E-complaint UBN</p>
+          </li>
+          <li>
+            <p>E-edu</p>
+          </li>
+          <li>
+            <p>School On The Internet</p>
+          </li>
+          <li>
+            <p>Jurnal UB</p>
+          </li>
+          <li>
+            <p>UB Forum</p>
+          </li>
+          <li>
+            <p>UB TV</p>
           </li>
         </ul>
-
       </div>
-      <!-- Grid column -->
+      </div>
 
-      <hr class="clearfix w-100 d-md-none">
-
-      <!-- Grid column -->
-      <div class="col-md-2 mx-auto">
-
+      <div class="col-md-3 mx-auto">
+      <div class="mx-3">
         <!-- Links -->
-        <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Faq</h5>
+        <h5 class="font-weight-bold text-uppercase mt-3">Kemahasiswaan</h5>
+        <hr >
 
         <ul class="list-unstyled">
           <li>
-            <p>Link 1</p>
+            <p>Email Mahasiswa</p>
           </li>
           <li>
-            <p>Link 1</p>
+            <p>Job Placements Center UB</p>
           </li>
           <li>
-            <p>Link 1</p>
+            <p>Online Schoolarship UB</p>
           </li>
           <li>
-           <p>Link 1</p>
+            <p>Pendaftaran</p>
+          </li>
+          <li>
+            <p>SIAM Online</p>
+          </li>
+          <li>
+            <p>BEM TI Vokasi UB</p>
+          </li>
+          <li>
+            <p>HIMATIF Vokasi UB</p>
           </li>
         </ul>
-
       </div>
-      <!-- Grid column -->
+      </div>
+
+      
+      <div class="col-md-3 mx-auto">
+      <div class="mx-3">
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mt-3">Blog</h5>
+        <hr >
+
+        <ul class="list-unstyled">
+          <li>
+            <p>UB Official Web</p>
+          </li>
+          <li>
+            <p>Prasetya Online</p>
+          </li>
+          <li>
+            <p>UB Web Mail</p>
+          </li>
+          <li>
+            <p>Blog Dosen</p>
+          </li>
+          <li>
+            <p>Blog Staff</p>
+          </li>
+          <li>
+            <p>Blog Mahasiswa</p>
+          </li>
+        </ul>
+      </div>
+      </div>
+
 
     </div>
     <!-- Grid row -->
-</div>
-  <!-- Footer Links -->
 
 
-  <!-- Social buttons -->
-  <ul class="list-unstyled list-inline text-center mt-5">
-    <li class="list-inline-item">
-      <a class="btn-floating btn-gplus mx-1" href="#">
-        <i class="fab fa-facebook-f"> </i>
-      </a>
-    </li>
-    <li class="list-inline-item">
-      <a class="btn-floating btn-gplus mx-1" href="#">
-        <i class="fab fa-twitter"> </i>
-      </a>
-    </li>
-    <li class="list-inline-item">
-      <a class="btn-floating btn-gplus mx-1" href="#">
-        <i class="fab fa-google-plus-g"> </i>
-      </a>
-    </li>
-    <li class="list-inline-item">
-      <a class="btn-floating btn-gplus mx-1" href="#">
-        <i class="fab fa-linkedin-in"> </i>
-      </a>
-    </li>
-    <li class="list-inline-item">
-      <a class="btn-floating btn-gplus mx-1" href="#">
-        <i class="fab fa-dribbble"> </i>
-      </a>
-    </li>
-  </ul>
-  <!-- Social buttons -->
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3 copy-background">© 2020 Copyright:
-  </div>
-  <!-- Copyright -->
-
+  
 </footer>
-<!-- Footer -->
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+
 </body>
 </html>
+
+
+
+
+
+
+
+<!-- <footer class="background">
+  <div class="container-fluid padding">
+    <div class="row">
+      <div class="col-md-4">
+        <h3>Layanan UB</h3>
+        <hr class="background-sec">
+          <p>Siam</p>
+          <p>Siam</p>
+          <p>Siam</p>
+          <p>Siam</p>
+      </div>
+
+      <div class="col-md-4">
+        <h3>Layanan UB</h3>
+        <hr class="background-sec">
+          <p>Siam</p>
+          <p>Siam</p>
+          <p>Siam</p>
+          <p>Siam</p>
+      </div>
+
+      <div class="col-md-4">
+        <h3>Layanan UB</h3>
+        <hr class="background-sec">
+          <p>Siam</p>
+          <p>Siam</p>
+          <p>Siam</p>
+          <p>Siam</p>
+      </div>
+      <div class="col-md-12">
+        <h3>Layanan UB</h3>
+        <hr class="background-sec">
+          <p>Siam</p>
+          <p>Siam</p>
+          <p>Siam</p>
+          <p>Siam</p>
+      </div>
+    </div>
+
+  </div>
+</footer> -->
